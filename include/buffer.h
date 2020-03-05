647c4cb2a63a8f9ab773ec1ef5e1764d6c3ed544
daniel <danielac1794@gmail.com>
2020-03-04 22:25:54 -0800

    Update buffer header with structs and declarations

--- include/buffer.h
+++ include/buffer.h
@@ -1,7 +1,40 @@
+#include <time.h>
+
 #ifndef PROYECTO1_BUFFER_H
 #define PROYECTO1_BUFFER_H
 
-//Buffer dummy stuff
-int get_buffer_int();
+//Buffer dumym code
+//Will delete until other dummy refs have been deleted
+int get_buffer_int()
+
+//Structs
+typedef struct
+{
+  int process_id;   //PID of process that wrote the message.
+  time_t time_of_creation;
+  int consumer_key  //Random key between 0 and 4 to end consumers with matching key
+} message_t;
+
+typedef struct {
+    message_t* const buffer;
+    int head;
+    int tail;
+    const int max;
+    bool full;
+} circular_buffer_t;
+
+circular_buffer_t circular_buffer_init(message_t* buffer, size_t size)
+
+int circular_buffer_put(circular_buffer_t* cbuffer, message_t message)
+
+int circular_buffer_get(circular_buffer_t* cbuffer, message_t* data);
+
+bool circular_buffer_empty(circular_buffer_t* cbuffer);
+
+bool circular_buffer_full(circular_buffer_t* cbuffer);
+
+size_t circular_buffer_capacity(circular_buffer_t* cbuffer);
+
+size_t circular_buffer_size(circular_buffer_t* cbuffer);
 
 #endif //PROYECTO1_BUFFER_H
