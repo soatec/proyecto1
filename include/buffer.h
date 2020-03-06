73c8550c220d48b9037a7672ebc4aa5e2d214bb1
daniel <danielac1794@gmail.com>
2020-03-05 18:51:24 -0800

    Add buffer init and buffer reset

--- include/buffer.h
+++ include/buffer.h
@@ -1,31 +1,35 @@
 #include <time.h>
+#include <stdbool.h>
 
 #ifndef PROYECTO1_BUFFER_H
 #define PROYECTO1_BUFFER_H
 
 //Buffer dumym code
 //Will delete until other dummy refs have been deleted
-int get_buffer_int()
+int get_buffer_int();
 
 //Structs
-typedef struct
+typedef struct message_t
 {
   int process_id;   //PID of process that wrote the message.
   time_t time_of_creation;
-  int consumer_key  //Random key between 0 and 4 to end consumers with matching key
+  int consumer_key;  //Random key between 0 and 4 to end consumers with matching key
 } message_t;
 
-typedef struct {
-    message_t* const buffer;
+typedef struct circular_buffer_t
+{
+    message_t* buffer;
     int head;
     int tail;
-    const int max;
     bool full;
+    int length;
 } circular_buffer_t;
 
-circular_buffer_t circular_buffer_init(message_t* buffer, size_t size)
+circular_buffer_t* circular_buffer_init(size_t size);
 
-int circular_buffer_put(circular_buffer_t* cbuffer, message_t message)
+int circular_buffer_put(circular_buffer_t* cbuffer, message_t message);
+
+void circular_buffer_reset(circular_buffer_t* cbuffer);
 
 int circular_buffer_get(circular_buffer_t* cbuffer, message_t* data);
 
--- src/buffer.c
+++ src/buffer.c
@@ -10,3 +10,26 @@ int get_buffer_int()
 {
     return buffer_int;
 }
+
+
+
+circular_buffer_t* circular_buffer_init(size_t size)
+{
+    circular_buffer_t* circular_buffer = (circular_buffer_t *) malloc(sizeof(circular_buffer_t));
+    circular_buffer->buffer = (message_t *) malloc (sizeof(message_t)*size);
+    if (!circular_buffer->buffer)
+    {
+        printf("Buffer init failed\n");
+    }
+    circular_buffer->length = size;
+    circular_buffer_reset(circular_buffer);
+
+    return circular_buffer;
+}
+
+void circular_buffer_reset(circular_buffer_t* cbuffer)
+{
+    cbuffer->head = 0;
+    cbuffer->tail = 0;
+    cbuffer->full = false;
+}
