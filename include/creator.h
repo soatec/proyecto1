2f13d003c455d3b82478a597289526d64693ccea
ladycat <gariasmontiel@gmail.com>
2020-03-08 17:16:35 -0600

    create strcuts

--- include/creator.h
+++ include/creator.h
@@ -1,8 +1,72 @@
 #ifndef PROYECTO1_CREATOR_H
 #define PROYECTO1_CREATOR_H
 
+
+#define N 5
+#define PRODUCERS 2
+#define CONSUMERS 4
+#define SHARED_PROCESS 1
+#define SHARED_THREATS 0
+/*
+este es el slot de struct usada como buffer (muts deleate later)
+*/
+typedef struct {
+  char *name;
+  char id[N];
+
+} item;
+typedef struct {
+    item **slots;
+    /*
+    A circular managment:
+    use the modulo operador to make the index next_in and next_out
+    */
+    int capacity;
+    int next_in; //keep track of where to produce the next item (N-1)
+    int next_out;//keep track of where to consume the next item (N-3)
+} buffer_head;
+
+
+
+/*******************************************************************
+* NAME : access_to_buffer_struct
+* DESCRIPTION :
+*           this struct implements the emaphores used
+*           to synchonize access to the buffer
+*
+* NOTES :
+*/
+typedef struct{
+  /*mutual exclusive: updates next_in next_out*/
+  sem_t mutex;
+  /*atomically counts the number of data items in the buffer */
+  sem_t data;
+  /*atomically counts the empty slots in the buffer (N)*/
+  sem_t empty; //N;
+
+} access_to_buffer_struct;
+
+
+/*******************************************************************
+* NAME : readers_and_writers_struct
+* DESCRIPTION :
+*         this struct implements the semaphores used
+*           to synchonize readers and writers
+*
+* NOTES :
+*/
+typedef struct{
+  /*mutual exclusion amount writers*/
+  sem_t wrt;
+  /*mutual exclusion uptades of readcount*/
+  sem_t mutex;
+  int readcount;
+}readers_and_writers_struct;
+
+
 //Creator dummy stuff
 char msg[10];
 int print_msg();
+
 
 #endif //PROYECTO1_CREATOR_H
--- src/creator.c
+++ src/creator.c
@@ -2,73 +2,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include "../include/creator.h"
-#include "../include/buffer.h"
-#include <sys/shm.h> //Memoria compartida
 #include <sys/ipc.h>
 #include <pthread.h>   /* pthread_... */
 #include <semaphore.h> /* sem_... */
-
-#define N 5
-#define PRODUCERS 2
-#define CONSUMERS 4
+#include <sys/mman.h>
+#include <fcntl.h>
+#include "../include/creator.h"
+#include "../include/buffer.h"
 /*
-este es el slot de struct usada como buffer (muts deleate later)
+A ring(or circular) buffer is a fixed-size buffer,
+which can overwrite new data to the beginning of the buffer when the buffer is full.
+The ring buffer can be simply implemented with an array and two indices
+  - one index points the beginning of the buffer,
+    and the other index denotes the end of the buffer.
+
+If the buffer is empty, the begin index equals to the end index(for simplicity, set them to 0).
+    When new data comes, push it to the entry of the end index,
+    and increase the end index.
+
+    At this moment, if the begin index equals to the end index,
+      then increase the begin index, too.
+
+    All of the index operations must modulo the size of the array, in case of buffer overflow.
+
+    reference : https://www.bo-yang.net/2016/07/27/shared-memory-ring-buffer
 */
-typedef struct {
-  char *name;
-  char id[N];
-
-} item;
-typedef struct {
-    item **slots;
-    /*
-    A circular managment:
-    use the modulo operador to make the index next_in and next_out
-    */
-    int capacity;
-    int next_in; //keep track of where to produce the next item (N-1)
-    int next_out;//keep track of where to consume the next item (N-3)
-} buffer_head;
-
-
-/*******************************************************************
-* NAME : access_to_buffer_struct
-* DESCRIPTION :
-*           this struct implements the emaphores used
-*           to synchonize access to the buffer
-*
-* NOTES :
-*/
-typedef struct{
-  /*mutual exclusive: updates next_in next_out*/
-  sem_t mutex;
-  int mutex_id;
-  /*atomically counts the number of data items in the buffer */
-  sem_t data;
-  int data_id;
-  /*atomically counts the empty slots in the buffer (N)*/
-  sem_t empty; //N;
-  int empty_id;
-
-} access_to_buffer_struct;
-
-
-/*******************************************************************
-* NAME : readers_and_writers_struct
-* DESCRIPTION :
-*         this struct implements the semaphores used
-*           to synchonize readers and writers
-*
-* NOTES :
-*/
-typedef struct{
-  /*mutual exclusion amount writers*/
-  sem_t wrt;
-  /*mutual exclusion uptades of readcount*/
-  sem_t mutex;
-  int readcount;
-}readers_and_writers_struct;
 
 
 /*******************************************************************
@@ -79,15 +37,17 @@ typedef struct{
 *     init next_out
 * INPUTS : n_slots of stols o capacidad el buffer
 * OUTPUTS : buffer_head struct
-* PROCESS :
+* PROCESS : If the buffer is empty, the begin index (update next_in)
+*                         equals to the end index (update next_out)
+*           (for simplicity, set them to 0).
 * NOTES :
 */
 buffer_head * init_buffer(int n_slots){
   buffer_head *buffer = malloc(sizeof *buffer);
     if (!buffer) return 0;
   buffer->capacity = n_slots;
-  buffer->next_in = 0;
-  buffer->next_out = 0;
+  buffer->next_in = 0;//keep track of where to produce the next item (N-1)
+  buffer->next_out = 0;//keep track of where to consume the next item (N-3)
   buffer->slots = malloc(n_slots * sizeof *(buffer->slots));
   return buffer;
 }
@@ -100,22 +60,20 @@ buffer_head * init_buffer(int n_slots){
 *     init numbers of stols
 * INPUTS : n_buffer or buffer size or number of stols
 * OUTPUTS : sync_access_to_buffer struct
-* PROCESS :
+* PROCESS : empty count the empty slots in the buffer and it's initialized with the total value.
 * NOTES :
 */
 access_to_buffer_struct * init_access_to_buffer_struct(int n_buffer){
     access_to_buffer_struct *struct_t;
     int id;
     struct_t = (access_to_buffer_struct *) malloc(sizeof(access_to_buffer_struct));
-    id = sem_init(&(struct_t->mutex), 0, 1);
-    struct_t->mutex_id = id;
-    id = sem_init(&(struct_t->data), 0, 0);
-    struct_t->data_id = id;
-    id = sem_init(&(struct_t->empty), 0, n_buffer);
-    struct_t->empty_id = id;
+    id = sem_init(&(struct_t->mutex), SHARED_PROCESS, 1);
+    id = sem_init(&(struct_t->data), SHARED_PROCESS, 0);// count the number of data items in the buffer
+    id = sem_init(&(struct_t->empty), SHARED_PROCESS, n_buffer);//count the empty slot in the buffer
     //struct_t = { .mutex= 1, .data = 0, .empy = n_buffer};//mutex,data,empty
     return struct_t;
 }
+
 
 /*******************************************************************
 * NAME : init_access_to_buffer
@@ -131,11 +89,60 @@ readers_and_writers_struct * init_readers_and_writers_struct (){
     //sync_readers_and_writers struct_t p1 = {1,1};//wrt, mutex
     readers_and_writers_struct *struct_t;
     struct_t = (readers_and_writers_struct *) malloc(sizeof(readers_and_writers_struct));
-    sem_init(&(struct_t->wrt), 0, 1);
-    sem_init(&(struct_t->mutex), 0, 1);
+    sem_init(&(struct_t->wrt), SHARED_PROCESS, 1);
+    sem_init(&(struct_t->mutex), SHARED_PROCESS, 1);
     struct_t->readcount = 0;
     return struct_t;
 }
+
+
+/*******************************************************************
+* NAME : add_data_index_buffer
+* DESCRIPTION :
+*    The code for adding new data into the buffer, is like to write information
+* into the buffer.
+*   update values of next_in next_out in order to manage the buffer
+* INPUTS :
+* OUTPUTS :
+* PROCESS : A - ring buffer array
+*             next_in - start index of the ring buffer
+*             next_out - end index of the ring buffer
+*             capacity - size of the ring buffer
+* NOTES :
+*/
+buffer_head*  add_data_index_buffer(item  data_item, buffer_head *buffer){
+  //change this line for add new information into the buffer
+  //buffer->slots[buffer->next_in]=data_item;
+  buffer->next_in = (buffer->next_in + 1)% buffer->capacity;
+  if (buffer->next_out == buffer->next_in);
+    buffer->next_out = (buffer->next_out + 1) % buffer->capacity;
+  return buffer;
+}
+
+
+/*******************************************************************
+* NAME : remove_data_index_buffer
+* DESCRIPTION :
+*    The code for adding new data into the buffer, is like to write information
+* into the buffer.
+*   update values of next_in next_out in order to manage the buffer
+* INPUTS :
+* OUTPUTS :
+* PROCESS : A - ring buffer array
+*             next_in - start index of the ring buffer
+*             next_out - end index of the ring buffer
+*             capacity - size of the ring buffer
+* NOTES :
+*/
+buffer_head*  remove_data_index_buffer(item  data_item, buffer_head *buffer){
+  //change this line for add new information into the buffer
+  //buffer->slots[buffer->next_in]=data_item;
+  buffer->next_out = (buffer->next_out + 1)% buffer->capacity;
+  if (buffer->next_out == buffer->next_in);
+    buffer->next_in = (buffer->next_in + 1) % buffer->capacity;
+  return buffer;
+}
+
 
 buffer_head *buffer;
 access_to_buffer_struct *access_to_buffer_head;
@@ -144,6 +151,35 @@ readers_and_writers_struct *readers_and_writers_t;
 pthread_t consumer_tid[CONSUMERS], producer_tid[PRODUCERS];
 
 
+int create_mmap(char* name, int shm_len){
+    int shm_fd;
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (shm_fd == -1){
+      perror("shm_open failed");
+      return false;
+    }
+
+
+    if (ftruncate(shm_fd, shm_len) == -1)
+      perror ("ftruncate");
+
+    void* ptr;
+    ptr = mmap(0, shm_len, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if ((ptr == (void *) -1)||(ptr == MAP_FAILED)) {
+        perror("mmap failed");
+        return false;
+    }
+
+
+    printf("%s", (char*)ptr);
+
+      /* Unmap shared memory */
+      munmap(ptr, shm_len);
+      /* Destroy shared memory */
+      shm_unlink(name);
+
+    return 0;
+}
 
 int print_msg()
 {
